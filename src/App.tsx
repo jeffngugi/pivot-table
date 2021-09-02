7cec477c67de5a7620f8b06eb0a26bdec6a1d554
jeffngugi <ngugimwathi254@gmail.com>
2021-09-02 15:57:05 +0300

    add and import json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useEffect, useState} from 'react'
 import './App.css';
+import Table from './components/Table';
 
-function App() {
+const App: React.FC=() => {
+
+  const [ salesOrders, setSalesOrders] =useState<[]>([])
+
+  const getOrders = async () =>{
+    await fetch("./data/sales-orders.json")
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      setSalesOrders(json)
+    });
+  }
+ 
+  useEffect(() => {
+      getOrders()
+  }, [])
+  // console.log(data)
+
+
+  // const 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table />
     </div>
   );
 }
---
+++ src/components/Table.tsx
@@ -0,0 +1,15 @@
+
+import './Table.css'
+
+const Table = () => {
+    return (
+        <section className='table-container'>
+           <table>
+ 
+</table>
+        </section>
+    )
+}
+
+export default Table
+
